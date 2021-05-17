30cc00bd364f8865d410bae5e3f5f8f71f42674b
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-05-17 22:49:04 +0900

    set testing for descending order

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
 describe 'タスクモデル機能', type: :model do
   let!(:task) { FactoryBot.create(:task, title: 'task', content: 'task') }
-  let!(:task2) { FactoryBot.create(:task, title: 'task2', content: 'task2') }
+  let!(:task2) { FactoryBot.create(:task, title: 'テスト用タスクタイトル', content: 'task2') }
   let!(:task3) { FactoryBot.create(:task, title: 'task3', content: 'task3') }
   before do
     visit tasks_path
@@ -12,7 +12,7 @@ describe 'タスクモデル機能', type: :model do
       it '作成したタスクが表示される' do
         visit new_task_path
         fill_in "タスク名", with: "task_name"
-        fill_in "詳細", with: "task_content"
+        fill_in "taks[content]", with: "task_content"
         click_on '登録'
         expect(page).to have_content 'task_name'
       end
@@ -21,7 +21,7 @@ describe 'タスクモデル機能', type: :model do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        expect(page).to have_content 'task'
+        expect(page).to have_content 'テスト用タスクタイトル'
       end
     end
   end
@@ -34,9 +34,12 @@ describe 'タスクモデル機能', type: :model do
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
-        visit task_path(task.id)
-         expect(page).to have_content 'task'
-        end
+        visit task_path(task2.id)
+        expect(page).to have_content 'テスト用タスクタイトル
+        expect(page).to have_content '詳細画面'
+      end
+    end
+  end
 
 
   
