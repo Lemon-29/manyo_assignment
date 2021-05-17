1d7bf02d61fc8b80c4980248ec2ff425fdd91340
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-05-17 22:25:21 +0900

    Testing for being sorted from descending order of made dates

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,23 +1,42 @@
 require 'rails_helper'
+RSpec.describe 'タスク管理機能', type: :system do
 describe 'タスクモデル機能', type: :model do
-  describe 'バリデーションのテスト' do
-    context 'タスクのタイトルが空の場合' do
-      it 'バリデーションにひっかる' do
-        task = Task.new(title: '', content: '失敗テスト')
-        expect(task).not_to be_valid
-      end
-    end
-    context 'タスクの詳細が空の場合' do
-      it 'バリデーションにひっかかる' do
-        task = Task.new(title: '失敗テスト2', content: '')
-        expect(task).not_to be_valid
-      end
-    end
-    context 'タスクのタイトルと詳細に内容が記載されている場合' do
-      it 'バリデーションが通る' do
-        task = Task.new(title: '成功テスト', content: '成功テスト')
-        expect(task).to be_valid
+  let!(:task) { FactoryBot.create(:task, title: 'task', content: 'task') }
+  let!(:task2) { FactoryBot.create(:task, title: 'task2', content: 'task2') }
+  let!(:task3) { FactoryBot.create(:task, title: 'task3', content: 'task3') }
+  before do
+    visit tasks_path
+  end
+  describe '新規作成機能' do
+    context 'タスクを新規作成した場合' do
+      it '作成したタスクが表示される' do
+        visit new_task_path
+        fill_in "タスク名", with: "task_name"
+        fill_in "詳細", with: "task_content"
+        click_on '登録'
+        expect(page).to have_content 'task_name'
       end
     end
   end
-end 
+  describe '一覧表示機能' do
+    context '一覧画面に遷移した場合' do
+      it '作成済みのタスク一覧が表示される' do
+        expect(page).to have_content 'task'
+      end
+    end
+  end
+  context 'タスクが作成日時の降順に並んでいる場合' do
+    it '新しいタスクが一番上に表示される' do
+      task_list = all('ul li')
+      expect(task_list[0]).to have_content 'task3'
+    end
+  end
+  describe '詳細表示機能' do
+     context '任意のタスク詳細画面に遷移した場合' do
+       it '該当タスクの内容が表示される' do
+        visit task_path(task.id)
+         expect(page).to have_content 'task'
+        end
+
+
+  
