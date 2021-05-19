53dba53eb6659b993d890ffe2faa2ca0560dd194
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-05-19 11:51:49 +0900

    set system spec sorting from order of last expiration period

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
-  let!(:task) { FactoryBot.create(:task, title: 'task', content: 'task') }
-  let!(:task2) { FactoryBot.create(:task, title: 'task2', content: 'task2') }
-  let!(:task3) { FactoryBot.create(:task, title: 'task3', content: 'task3') }
+  let!(:task) { FactoryBot.create(:task, title: 'task', content: 'task', expired_at: '2021-06-03 03:33:33') }
+  let!(:task2) { FactoryBot.create(:task, title: 'task2', content: 'task2', expired_at: '2021-06-06 03:33:33') }
+  let!(:task3) { FactoryBot.create(:task, title: 'task3', content: 'task3', expired_at: '2021-06-16 03:33:33') }
   before do
     visit tasks_path
   end
@@ -20,9 +20,9 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
+        expect(page).to have_content 'task2'
         task = FactoryBot.create(:task, title: 'task')
         visit tasks_path
-        expect(page).to have_content 'task'
       end
     end
   end
@@ -30,6 +30,16 @@ RSpec.describe 'タスク管理機能', type: :system do
     it '新しいタスクが一番上に表示される' do
       task_list = all('ul li')
       expect(task_list[0]).to have_content 'task3'
+    end
+  end
+  context 'タスクが終了期限の降順に並んでいる場合' do
+    it '終了期限の遅いタスクが一番上に表示される' do
+      within '.sort_expired' do
+        click_on '終了期限でソートする'
+      end
+      # binding.pry
+      task_list = all('ul li')
+      expect(task_list[0]).to have_content 'task2'
     end
   end
   describe '詳細表示機能' do
