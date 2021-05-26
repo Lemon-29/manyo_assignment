f9ff63b686ffe37cddd2de8575212b7a8536db66
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-05-26 13:45:05 +0900

    set user and association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_21_080245) do
+ActiveRecord::Schema.define(version: 2021_05_26_043136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(version: 2021_05_21_080245) do
     t.datetime "updated_at", null: false
     t.integer "priority"
     t.date "expired_at"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "tasks", "users"
 end
