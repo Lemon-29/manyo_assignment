ee784ca94998855f4ba198d99aceefe22f820cab
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-05-27 11:19:45 +0900

    set migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   resources :tasks
-  resources :users
+  resources :users do
+    collection do
+    end
+  end
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   root 'tasks#index'
-  collection do
-  
 end
 
-
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html``` 
-
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_26_043136) do
+ActiveRecord::Schema.define(version: 2021_05_27_021704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,9 +31,10 @@ ActiveRecord::Schema.define(version: 2021_05_26_043136) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "name"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "admin"
   end
 
   add_foreign_key "tasks", "users"
